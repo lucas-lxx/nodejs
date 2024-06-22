e60ab4ea7cafadc886c7d654e5e065d6dd4d6b2a
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-06-22 16:30:22 -0300

    refactor: improve path utils

--- section-5:Working-with-Express.js/app.js
+++ section-5:Working-with-Express.js/app.js
@@ -3,20 +3,20 @@ const path = require('path');
 const express = require('express');
 const body_parser = require('body-parser')
 
-const admin_router = require('./routes/admin');
+const admin_data = require('./routes/admin');
 const shop_router = require('./routes/shop');
 const home_router = require('./routes/home');
 const route_not_found = require('./routes/404');
-const root_dir_path = require('./util/path');
+const { public_dir_path } = require('./util/path');
 
 const port = 3000;
 
 const app = express();
 
 app.use(body_parser.urlencoded({extended: true}));
-app.use(express.static(path.join(root_dir_path, 'public')));
+app.use(express.static(public_dir_path));
 
-app.use(admin_router);
+app.use(admin_data.router);
 app.use('/shop', shop_router);
 app.use(home_router);
 app.use(route_not_found);
--- section-5:Working-with-Express.js/util/path.js
+++ section-5:Working-with-Express.js/util/path.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
 const root_dir_path = path.dirname(require.main.filename);
+const view_dir_path = path.join(root_dir_path, 'view');
+const routes_dir_path = path.join(root_dir_path, 'routes');
+const public_dir_path = path.join(root_dir_path, 'public');
 
-module.exports = root_dir_path;
+module.exports = {
+  root_dir_path,
+  view_dir_path,
+  routes_dir_path
+}
