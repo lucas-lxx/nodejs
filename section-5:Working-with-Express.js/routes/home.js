d30645157055420ca4837d167d316569fe919e48
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-06-22 16:20:07 -0300

    chore: added home screen

--- section-5:Working-with-Express.js/routes/home.js
+++ section-5:Working-with-Express.js/routes/home.js
@@ -3,9 +3,11 @@ const path = require('path');
 const router = require('express').Router();
 
 const root_dir_path = require('../util/path');
+const {products} = require('../routes/admin')
 
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(root_dir_path, 'views', 'home.html'))
+  console.log(products)
 });
 
 module.exports = router;
