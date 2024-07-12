bf130e4eb4614607ee0d0cacb55b60f67482f562
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-07-12 09:21:01 -0300

    chore: remove unused code

--- section-7:the-model-view-controller-MVC/routes/404.js
+++ section-7:the-model-view-controller-MVC/routes/404.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const root_dir_path = require('../util/path');
-
 const router = (req, res, next) => {
   res.status(404).render('404', {
     pageTitle: 'CatShop is Not Here :/',
