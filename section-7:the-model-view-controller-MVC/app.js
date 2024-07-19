a2bd0248c364125f8f8b2cbe11a63befc081a543
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-07-19 15:03:51 -0300

    refactor: merge home and products routes into home

--- section-7:the-model-view-controller-MVC/app.js
+++ section-7:the-model-view-controller-MVC/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const body_parser = require('body-parser')
 
 const admin_router = require('./routes/admin');
-const shop_router = require('./routes/products');
 const home_router = require('./routes/home');
 const errorController = require('./controllers/error');
 const { public_dir_path } = require('./util/path');
@@ -16,9 +15,8 @@ app.set('views', 'views');
 app.use(body_parser.urlencoded({extended: true}));
 app.use(express.static(public_dir_path));
 
+app.use('/', home_router);
 app.use('/admin', admin_router);
-app.use('/products', shop_router);
-app.use(home_router);
 app.use(errorController.get404);
 
 app.listen(port, '0.0.0.0');
--- section-7:the-model-view-controller-MVC/routes/home.js
+++ section-7:the-model-view-controller-MVC/routes/home.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const homeController = require('../controllers/home');
+const productsController = require('../controllers/products');
 
 router.get('/', homeController.getHome);
+
+router.get('/products', productsController.getProducts);
 
 module.exports = router;
