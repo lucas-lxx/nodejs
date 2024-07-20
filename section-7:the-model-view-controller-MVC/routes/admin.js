ea3deb7a2a574df09be16be83e09b83f01be0c0f
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-07-20 12:47:10 -0300

    refactor: order the http verbs

--- section-7:the-model-view-controller-MVC/routes/admin.js
+++ section-7:the-model-view-controller-MVC/routes/admin.js
@@ -3,11 +3,11 @@ const express = require('express');
 const productsController = require('../controllers/products');
 
 const router = express.Router();
-
+// /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
-
-router.post('/product', productsController.postAddProduct);
-
+// /admin/products => GET
 router.get('/products', productsController.getAdminProducts);
+// /admin/product => POST
+router.post('/product', productsController.postAddProduct);
 
 module.exports = router;
