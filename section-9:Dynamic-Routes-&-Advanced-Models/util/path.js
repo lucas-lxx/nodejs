96ed3a1d5ff759bc73dbacb5385db3a80549430f
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-08-02 12:18:35 -0300

    chore: update spacing

--- section-9:Dynamic-Routes-&-Advanced-Models/util/path.js
+++ section-9:Dynamic-Routes-&-Advanced-Models/util/path.js
@@ -4,7 +4,7 @@ const root_dir_path = path.dirname(require.main.filename);
 const views_dir_path = path.join(root_dir_path, 'views');
 const routes_dir_path = path.join(root_dir_path, 'routes');
 const public_dir_path = path.join(root_dir_path, 'public');
-const data_dir_path =path.join(root_dir_path, 'data');
+const data_dir_path = path.join(root_dir_path, 'data');
 
 module.exports = {
   root_dir_path,
