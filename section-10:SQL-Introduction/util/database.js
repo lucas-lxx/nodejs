1c6247b82a317a52260478858965e6ab5fdad309
Lucas Almeida <lucas.lxx@alu.ufc.br>
2024-08-24 22:02:27 -0300

    feat: add mysql pool connection

--- section-10:SQL-Introduction/util/database.js
+++ section-10:SQL-Introduction/util/database.js
@@ -1,1 +1,10 @@
 const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'lucas',
+    database: 'node_complete',
+    password: 'Ognms1011##'
+})
+
+module.exports = pool.promise();
